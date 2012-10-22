fb8bdfce32e692c4d5929993c623cd2e78bcb858
Mihaela Rosca <mihaelacrosca@gmail.com>
2012-10-23 00:21:18 +0100

    Created random generator with range

--- src/Util.hs
+++ src/Util.hs
@@ -1,15 +1,23 @@
 module Util (
     repeatUntilEqual
   , (./.)
+  , getRandom
   , (*.)
 ) where
 
+import System.Random
 
 (./.) :: (Fractional a, Integral a1, Integral a2) => a1 -> a2 -> a
 x ./. y = fromIntegral x / fromIntegral y
 
 (*.) :: (Integral a1, Num a) => a -> a1 -> a
 x *. y = x * fromIntegral y
+
+
+getRandom :: Int -> Int -> IO Int
+getRandom lower upper = do
+  r <- randomIO
+  return $ abs (lower + r `mod` (upper - lower))
 
 
 repeatUntilEqual :: (Eq a) => (a -> a) -> a -> a
