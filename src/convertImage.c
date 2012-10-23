7b3e39737d34535b2dfff047078e1281fefa6894
Razvan <rvm10@imperial.ac.uk>
2012-10-23 22:10:13 +0100

    mapping from double to binary done + loading from command line

--- src/convertImage.c
+++ src/convertImage.c
@@ -10,9 +10,25 @@
     exit(-1); \
   } \
 
+/* converts a list of doubles to binary + flattens the matrix to a vector */
+int* 
+mapToBinary(double** pattern, int width, int height){
+  int* binaryPattern =(int*) malloc(sizeof(int) * width * height);
+  int i=0;
 
-void test_wand(void)
+  for(int w = 0; w < width; w++)
+    for(int h = 0; h < height; h++)
+    {
+      binaryPattern[i] = pattern[w][h] < 0.5 ? 0 : 1;
+      i++;
+    }    
+
+  return binaryPattern;
+}
+
+int* load_picture(char* inputImg)
 {
+  /* load a picture in the "wand" */
   MagickWand *mw = NULL;
 
   MagickWandGenesis();
@@ -21,9 +37,7 @@ void test_wand(void)
   mw = NewMagickWand();
 
   /* Read the input image */
- char* inputImg = "../images/black3.jpg";
  MagickBooleanType retVal = MagickReadImage(mw, inputImg);
- printf("%s", inputImg);
 
    if (retVal == MagickFalse) 
      ThrowWandException(mw); 
@@ -33,16 +47,25 @@ void test_wand(void)
   size_t width = MagickGetImageWidth(mw);
   size_t height = MagickGetImageHeight(mw);
   long y;
-  double outputPattern[width][height];
-
+  double** outputPattern = (double**) malloc (sizeof(double*) * width);
+  for(int i=0; i < width; i++)
+  {
+    outputPattern[i] = (double*) malloc(sizeof(double) * height);
+  }
+  
+  /* get pixel grayscale values */
   for (y=0; y < (long) height; y++) 
   { 
     pixels=PixelGetNextIteratorRow(pixelIt,&width);
     for (long x=0; x < (long) width; x++) { 
-      outputPattern[x][y] = PixelGetBlack(pixels[x]); 
+      outputPattern[x][y] = (PixelGetRed(pixels[x]) + 
+        PixelGetGreen(pixels[x]) + PixelGetBlue(pixels[x]))/3; 
     }
   }
 
+  return mapToBinary(outputPattern, width, height);
+
+  /* print the vector */
   for (y=0; y < (long) height; y++){ 
     for (long x=0; x < (long) width; x++) 
       printf("%f ", outputPattern[x][y]); 
@@ -50,17 +73,24 @@ void test_wand(void)
     printf("\n\n\n");
   }
 
- /* write it */
-  //MagickWriteImage(mw,"./black_out1.jpg");
-
   /* Tidy up */
   if(mw) mw = DestroyMagickWand(mw);
 
   MagickWandTerminus();
 }
 
-int main(){
+int main(int argc, char** args){
+
+  //char* imageInput = "../images/3x3.bmp"; 
+  if(args[1] != NULL)
+  {  
+    int* finalPattern = load_picture(args[1]); 
+    for(int i = 0; i < 9; i++)
+      printf("%d ", finalPattern[i]);
+  }
+  else
+    printf("FREEZE!!! no image input provided!!! ");
   
-  test_wand();
+
   return 0;
 }
