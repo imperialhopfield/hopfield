53b95df62f3abc8ed336bba8ecd4323e0a519e58
Wael Al Jishi <wa910@doc.ic.ac.uk>
2012-10-24 14:00:52 +0100

    Added comment to address testcase

--- test/Main.hs
+++ test/Main.hs
@@ -14,6 +14,8 @@ main = hspec $ do
 
       describe "buildHopfieldData" $ do
 
+         --TODO limit pattern size! After around 19 tests,
+         --computer slows down to a halt due to high memory usage
          it "trains an all-positive pattern correctly" $
            forAll ((sameElemVector 1) `suchThat` (not . V.null))
              (\pat -> weights (buildHopfieldData [pat]) == allOnesWeights (V.length pat))
