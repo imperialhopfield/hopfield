e54a0eb39f6dc83ac9c2d1a5daa77ea09f8effd7
Wael Al Jishi <wa910@doc.ic.ac.uk>
2012-10-29 17:54:48 +0000

    Deleted commented out test which already exists

--- test/Main.hs
+++ test/Main.hs
@@ -29,9 +29,6 @@ main = hspec $ do
 
          --TODO limit pattern size! After around 19 tests,
          --computer slows down to a halt due to high memory usage
-         --it "trains an all-positive pattern correctly" $
-         --  forAll ((sameElemVector 1) `suchThat` (not . V.null))
-         --    (\pat -> weights (buildHopfieldData [pat]) == allOnesWeights (V.length pat))
       it "tests that the patterns stored in the hopfield datastructure are the same as the ones which were given as input" $
         forAll (patternListGen `suchThat` (not . null))
           (\pats -> (patterns $ buildHopfieldData pats) == pats)
