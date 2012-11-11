81eb42f20b88ed4ddc41892777865a1395a670f4
Razvan Marinescu <rvm10@imperial.ac.uk>
2012-11-11 18:27:04 +0000

    added resizeToMin, load_into_wand and resize multiple images

--- src/convertImage.c
+++ src/convertImage.c
@@ -1,5 +1,12 @@
 #include "convertImage.h"
 #include <wand/magick_wand.h>
+#include <stdbool.h>
+#include <limits.h>
+
+size_t minH = 10;
+size_t maxH = 50;
+size_t minW = 10;
+size_t maxW = 50;
 
 void ThrowWandException(MagickWand *wand)
   {
@@ -12,12 +19,14 @@ void ThrowWandException(MagickWand *wand)
   }
 
 /* converts a list of doubles to binary + flattens the matrix to a vector */
-binary_pattern_t *
+static binary_pattern_t *
 mapToBinary(double** pattern, int width, int height){
-  binary_pattern_t* binaryPattern = (binary_pattern_t *) malloc(sizeof(binaryPattern));
+  binary_pattern_t* binaryPattern = 
+    (binary_pattern_t *) malloc(sizeof(binaryPattern));
   const int size = width * height;
   binaryPattern->size = size;
-  binaryPattern->pattern = (uint32_t *) malloc(sizeof(binaryPattern->pattern) * size);
+  binaryPattern->pattern = 
+    (uint32_t *) malloc(sizeof(binaryPattern->pattern) * size);
 
   int i=0;
 
@@ -31,11 +40,11 @@ mapToBinary(double** pattern, int width, int height){
   return binaryPattern;
 }
 
-
-binary_pattern_t *
-load_picture(char* inputImg, size_t width, size_t height)
+/* loads an image into a magic wand */
+MagickWand*
+load_into_wand(char* inputImg)
 {
-  /* load a picture in the "wand" */
+   /* load a picture in the "wand" */
   MagickWand *mw = NULL;
 
   MagickWandGenesis();
@@ -46,14 +55,73 @@ load_picture(char* inputImg, size_t width, size_t height)
   /* Read the input image */
  MagickBooleanType retVal = MagickReadImage(mw, inputImg);
 
-   if (retVal == MagickFalse)
+   if (retVal == MagickFalse){
      ThrowWandException(mw);
+     return NULL;
+   }
+    
+  return mw;  
+}
 
+/* takes a magic wand object and resizes it */
+static bool
+resize(MagickWand* imgWand, size_t width, size_t height)
+{
+  return MagickResizeImage(imgWand, height, width, LanczosFilter, 0);
+}
+
+/* resizes a list of images to the minimum of the widths and 
+   heights across the images. Furthermore, if the values are
+   not in the range min, max, then we shall scale them so that
+   they fit in the range as well */
+MagickWand**
+resizeToMin(MagickWand** imgWands, int nr)
+{
+  size_t imgMinW = INT_MAX, imgMinH = INT_MAX; 
+
+  /* calculate absolute min width and height for the images */
+  for(int i = 0; i < nr; i++)
+  {
+    if (MagickGetImageWidth(imgWands[i]) < imgMinW)
+      imgMinW = MagickGetImageWidth(imgWands[i]); 
+    if (MagickGetImageHeight(imgWands[i]) < imgMinH)
+      imgMinH = MagickGetImageWidth(imgWands[i]); 
+  }
+
+  /*if the min values don't fit in the range, trim down even more*/
+  /* rescale width */ 
+ if(imgMinW > maxW) 
+    imgMinW = maxW;
+  if(imgMinW < minW) 
+    imgMinW = minW;
+
+  /* rescale height */
+  if(imgMinH > maxH) 
+    imgMinH = maxH;
+  if(imgMinH < minH) 
+    imgMinH = minH;
+
+  for(int i = 0; i < nr; i++)
+  {
+    resize(imgWands[i], imgMinW, imgMinH);
+  }
+
+  return NULL;
+}
+
+
+
+/* converts a magic wand to a binary pattern. it first converts it to a
+   list of doubles, and then used the helper function mapToBinary in 
+   order to convert the doubles to binary */
+binary_pattern_t*
+mwToBinary(MagickWand* imgWand)
+{
   PixelWand** pixels;
-  PixelIterator* pixelIt = NewPixelIterator(mw);
-  // size_t width = MagickGetImageWidth(mw);
-  // size_t height = MagickGetImageHeight(mw);
-  MagickResizeImage(mw, height, width, LanczosFilter, 0);
+  PixelIterator* pixelIt = NewPixelIterator(imgWand);
+ 
+  size_t width  = MagickGetImageWidth(imgWand); 
+  size_t height = MagickGetImageHeight(imgWand); 
   double** outputPattern = (double**) malloc (sizeof(double*) * width);
   for(size_t i=0; i < width; i++)
   {
@@ -71,18 +139,15 @@ load_picture(char* inputImg, size_t width, size_t height)
     }
   }
 
+  /* Tidy up */
+  if(imgWand) 
+    imgWand = DestroyMagickWand(imgWand);
+
+  //MagickWandTerminus();
+
   return mapToBinary(outputPattern, width, height);
 
-  /* print the vector */
-  for (y=0; y < (long) height; y++){
-    for (long x=0; x < (long) width; x++)
-      printf("%f ", outputPattern[x][y]);
-
-    printf("\n\n\n");
-  }
-
-  /* Tidy up */
-  if(mw) mw = DestroyMagickWand(mw);
-
-  MagickWandTerminus();
 }
+
+
+
