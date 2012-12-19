5e74b898ef5e14fc09ce0b88fdf8f0a32336e8ce
Mihaela Rosca <mihaelacrosca@gmail.com>
2012-12-19 12:53:34 +0000

    comments and main

--- src/BolzmannMachine.hs
+++ src/BolzmannMachine.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE PatternGuards #-}
+
 module BolzmannMachine where
+-- | Base Restricted Bolzamann machine.
+
+-- http://en.wikipedia.org/wiki/Restricted_Boltzmann_machine
 
 import           Control.Monad
 import           Control.Monad.Random
@@ -9,7 +14,11 @@ import qualified Numeric.Container as NC
 import Hopfield
 import Util
 
--- start with no biases initially, introduce them after
+-- In the case of the Bolzamann Machine the weight matrix establishes the
+-- weigths between visible and hidden neurons
+-- w i j - connection between visible neuron i and hidden neuron j
+
+-- start with no biases initially, introduce them after (if needed)
 
 lr = 0.1 :: Double -- learning rate
 
@@ -40,32 +49,37 @@ updateVisible ws h index = do
 
 
 getHidden:: MonadRandom m => Weights -> Pattern -> m Pattern
-getHidden ws v = do
-  h <- mapM (updateHidden ws v) [0.. (V.length $ ws ! 0) - 1]
-  return $ V.fromList h
+getHidden ws v =
+  | Just e <- validHiddenPattern ws h  = error e
+  | otherwise = do
+      h <- mapM (updateHidden ws v) [0.. (V.length $ ws ! 0) - 1]
+      return $ V.fromList h
 
 
 getVisible:: MonadRandom m => Weights -> Pattern -> m Pattern
-getVisible ws h = do
-  v <- mapM (updateVisible ws h) [0.. (V.length ws) - 1]
-  return $ V.fromList v
+getVisible ws h
+  | Just e <- validHiddenPattern ws h  = error e
+  | otherwise = do
+      v <- mapM (updateVisible ws h) [0.. (V.length ws) - 1]
+      return $ V.fromList v
 
 
 updateWS:: MonadRandom m => Weights -> Pattern -> m Weights
 updateWS ws v = do
-  h    <- getHidden ws v
-  v'   <- getVisible ws h
-  h'   <- getHidden ws v'
-  let v_d  = V.fromList $ map fromIntegral (V.toList v)
-  let v_d' = V.fromList $ map fromIntegral (V.toList v')
-  let h_d  = V.fromList $ map fromIntegral (V.toList h)
-  let h_d' = V.fromList $ map fromIntegral (V.toList h')
-  let pos  = NC.toLists $ (fromDataVector v_d) `NC.outer` (fromDataVector h_d)
-  let neg  = NC.toLists $ (fromDataVector v_d') `NC.outer` (fromDataVector h_d')
-  let dws  = combine (-) pos neg
-  let ws_f = combine (+) (list2D ws) dws
+  h         <- getHidden ws v
+  v'        <- getVisible ws h
+  h'        <- getHidden ws v'
+  let v_d   = V.fromList $ map fromIntegral (V.toList v)
+  let v_d'  = V.fromList $ map fromIntegral (V.toList v')
+  let h_d   = V.fromList $ map fromIntegral (V.toList h)
+  let h_d'  = V.fromList $ map fromIntegral (V.toList h')
+  let pos   = NC.toLists $ (fromDataVector v_d) `NC.outer` (fromDataVector h_d)
+  let neg   = NC.toLists $ (fromDataVector v_d') `NC.outer` (fromDataVector h_d')
+  let dws   = combine (-) pos neg
+  let ws_f  = combine (+) (list2D ws) dws
   let final = map (map (\x -> x * lr)) ws_f
   return $ vector2D final
+
 
 train :: MonadRandom m => [Pattern] -> Int -> m Weights
 train pats nr_hidden = do
@@ -79,11 +93,29 @@ activation :: Double -> Double
 activation x = 1.0 / (1.0 - exp (-x))
 
 
--- stack overflow code
+-- | @validPattern weights pattern@
+-- Returns an error string in a Just if the @pattern@ is not compatible
+-- with @weights@ and Nothing otherwise.
+validHiddenPattern :: Weights -> Pattern -> Maybe String
+validHiddenPattern ws h
+  | V.length $ ws ! 0 /= V.length h = Just "Size of hidden must match network size"
+  | otherwise                   = Nothing
+
+
+-- | @validPattern weights pattern@
+-- Returns an error string in a Just if the @pattern@ is not compatible
+-- with @weights@ and Nothing otherwise.
+validVisiblePattern :: Weights -> Pattern -> Maybe String
+validVisiblePattern ws v
+  | V.length ws /= V.length v = Just "Size of visible pattern must match network size"
+  | otherwise                   = Nothing
+
+
+-- TODO move to Util
 
 -- |Generates uniform random variables.
 unif :: (MonadRandom m) => m Double
-unif = getRandomR (0,1)
+unif = getRandomR (0, 1)
 
 -- |Generate two samples from the standard normal distribution, using
 --  the Box-Muller method.
@@ -100,7 +132,7 @@ stdNormals = do
 --  generating two samples and throwing away the second one.
 stdNormal :: (MonadRandom m) => m Double
 stdNormal = do
-  (x,_) <- stdNormals
+  (x, _) <- stdNormals
   return x
 
 -- |Generate a sample from the standard normal distribution with a given
@@ -109,3 +141,15 @@ normal :: (MonadRandom m) => Double -> Double -> m Double
 normal mu sigma = do
   x <- stdNormal
   return $ mu + sigma * x
+
+
+
+
+main = do
+  gen  <- getStdGen
+  let v1 = V.fromList [0, 1, 0]
+  let v2 = V.fromList [1, 0, 0]
+  let v3 = V.fromList [1, 1, 0]
+  let v4 = V.fromList [0, 0, 0]
+  return $ evalRand (train [v1, v2, v3, v4] 4) gen
+
