b7501aef233207280f6e4b81fdee859df912968b
Mihaela Rosca <mihaelacrosca@gmail.com>
2012-12-22 13:13:22 +0000

    more comments

--- src/BolzmannMachine.hs
+++ src/BolzmannMachine.hs
@@ -59,6 +59,8 @@ getCounterPattern mode ws pat
       return $ V.fromList c_pat
 
 
+-- | One step in updates the weigths in the training process.
+-- The weights are changed according to one of the training patterns.
 updateWS:: MonadRandom m => Weights -> Pattern -> m Weights
 updateWS ws v = do
   h         <- getCounterPattern Visible ws v
@@ -73,6 +75,8 @@ updateWS ws v = do
   return $ vector2D ws_l
 
 
+-- | The training function for the BolzmannMachine.
+-- We are using the contrastive divergence algorithm.
 -- CD-1 (we could extend to CD-n)
 train :: MonadRandom m => [Pattern] -> Int -> m Weights
 train pats nr_hidden = do
@@ -83,13 +87,16 @@ train pats nr_hidden = do
       nr_visible = V.length $ pats !! 0
 
 
+
+-- | The activation functiom for the network (the logistic sigmoid).
 activation :: Double -> Double
 activation x = 1.0 / (1.0 - exp (-x))
 
 
--- | @validPattern weights pattern@
+-- | @validPattern mode weights pattern@
 -- Returns an error string in a Just if the @pattern@ is not compatible
--- with @weights@ and Nothing otherwise.
+-- with @weights@ and Nothing otherwise. @mode@ gives the type of the pattern,
+-- which is checked (Visible or Hidden)
 validPattern :: Mode -> Weights -> Pattern -> Maybe String
 validPattern mode ws pat
   | getDimension mode ws /= (V.length pat) = Just "Size of hidden must match network size"
@@ -103,11 +110,15 @@ normal m std = do
   r <- DR.runRVar (DR.normal m std) (getRandom :: MonadRandom m => m Word32)
   return r
 
+-- | Does one update of a visible pattern by updating the hidden layer neurons once
+-- and then using the new values to obtain new values for the visible layer.
 update1 :: MonadRandom m => Weights -> Pattern -> m Pattern
 update1 ws pat = do
   h <- getCounterPattern Visible ws pat
   getCounterPattern Hidden ws h
 
+-- | As in the case of the Hopfield network, repeates an update until the pattern
+-- converges.
 repeatedUpdate1 :: MonadRandom m => Weights -> Pattern -> m Pattern
 repeatedUpdate1 ws pat = repeatUntilEqual (update1 ws) pat
 
