32309fd57ac2a6f3758a5c5a4099140b6380eefd
Wael Al Jishi <wa910@doc.ic.ac.uk>
2013-01-03 20:19:22 +0300

    Updated experiment file

--- src/Experiment.hs
+++ src/Experiment.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE ParallelListComp #-}
+
 module Main where
 
+import Analysis
 import Control.Monad (replicateM)
+import Data.List
 import Hopfield
 import Measurement
 import Test.QuickCheck
@@ -13,51 +17,58 @@ import Utils (Type(H), patternGen)
 
 genIO :: Gen a -> IO a
 genIO g = do
-	rndInt <- randomIO
-	stdGen <- getStdGen
-	return $ unGen g stdGen rndInt
+    rndInt <- randomIO
+    stdGen <- getStdGen
+    return $ unGen g stdGen rndInt
+
+
+attachLabels :: (Show a, Show b) => [a] -> [b] -> [Char]
+attachLabels labels items
+  = concat [ concat [show l, ": ", show i, "\n"] | l <- labels | i <- items ]
 
 
 main :: IO ()
 main = do
 
-	let n          = 25
-	    numRandoms = 8
-	    maxDegree  = 8
+    let n          = 50
+        numRandoms = 8
+        maxDegree  = 8
 
 
-	-- The super attractor - primary care giver
-	originPat <- genIO $ patternGen H n
+    -- The super attractor - primary care giver
+    originPat <- genIO $ patternGen H n
 
-	-- Sample random patterns - noise data
-	randomPats <- replicateM numRandoms $ genIO $ patternGen H n
-
-
-	let pats = originPat:randomPats
-
-	let degrees = powersOfTwo maxDegree
+    -- Sample random patterns - noise data
+    randomPats <- replicateM numRandoms $ genIO $ patternGen H n
 
 
-	-- Original pattern as the sole pattern in the network
-	putStrLn "Building networks"
-	let nets = buildNetworks pats degrees oneSuperAttr
+    let pats        = originPat:randomPats
+        originIndex = 0                         -- index of main pattern
+        degrees     = powersOfTwo maxDegree
+        patCombiner = oneSuperAttr
 
 
-	putStrLn "Origin fixed?"
-	print $ map (\n -> checkFixed n 0) $ map snd nets
+    putStrLn $ "Expected network errors: "
+    let expErrs = [ computeErrorSuperAttractorNumbers d (length pats) n | d <- degrees ]
+    putStrLn $ attachLabels degrees expErrs
 
 
-	putStrLn "Test fixed"
-	let (_,n1):_ = nets
-	ex <- samplePatternRing n1 originPat 0
-	print ex
+    -- Original pattern as the sole pattern in the network
+    putStrLn "Building networks..."
+    let nets = buildNetworks pats degrees patCombiner
 
-	putStrLn "Sampling basin"
-	measures <- samplePatternBasin n1 originPat
-	print measures
 
-	putStrLn "Measuring basins of attraction"
-	result <- measureMultiBasins measurePatternBasin nets originPat
+    --Check if pattern is fixed.
+    let patErrs = [ n | n <- nets, checkFixed n 0]
+    if not $ null patErrs
+        then putStrLn $
+        -- TODO FIX TO RETURN DEGREES NOT NETWORKS
+            "WARNING: The following degrees have produced networks where the pattern is NOT a fixed point:\n" ++
+              show patErrs ++ "\n"
+        else putStrLn "Pattern is always a fixed point\n"
 
-	-- Output: (Degree of super attractor, basin of attraction size)
-	print result
+
+    putStrLn "Measuring basins of attraction"
+    results <- measureMultiBasins measurePatternBasin nets originPat
+
+    putStrLn $ attachLabels degrees results
