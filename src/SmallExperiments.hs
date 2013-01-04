1e7852e45389ef74c8027f5f57c1210fc64a30ba
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-04 21:52:24 +0000

    Different numbers in small experiments

--- src/SmallExperiments.hs
+++ src/SmallExperiments.hs
@@ -11,18 +11,18 @@ import Util
 main :: IO ()
 main = do
 
-  putStrLn "basinsGivenStdT2 Hebbian 50 4 25 5"
-  avg1 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 4 25 5)
+  putStrLn "basinsGivenStdT2 Hebbian 50 6 25 10"
+  avg1 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 6 25 10)
   print avg1
 
-  putStrLn "basinsGivenStdT2 Storkey 50 4 25 5"
-  avg2 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 4 25 5)
+  putStrLn "basinsGivenStdT2 Storkey 50 6 25 10"
+  avg2 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 6 25 10)
   print avg2
 
-  putStrLn "basinsGivenStdT2 Hebbian 50 6 25 10"
-  avg3 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 6 25 10)
+  putStrLn "basinsGivenStdT2 Hebbian 50 4 25 10"
+  avg3 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 4 25 10)
   print avg3
 
-  putStrLn "basinsGivenStdT2 Storkey 50 6 25 10"
-  avg4 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 6 25 10)
+  putStrLn "basinsGivenStdT2 Storkey 50 4 25 10"
+  avg4 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 4 25 10)
   print avg4
