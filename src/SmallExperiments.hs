19ff54ed541d7a1cc8255ff10adb2218a66688de
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-04 21:58:42 +0000

    Changed numbers in experiments again.

--- src/SmallExperiments.hs
+++ src/SmallExperiments.hs
@@ -12,11 +12,11 @@ main :: IO ()
 main = do
 
   putStrLn "basinsGivenStdT2 Hebbian 50 6 25 10"
-  avg1 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 6 25 10)
+  avg1 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 6 25 5)
   print avg1
 
   putStrLn "basinsGivenStdT2 Storkey 50 6 25 10"
-  avg2 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 6 25 10)
+  avg2 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 6 25 5)
   print avg2
 
   putStrLn "basinsGivenStdT2 Hebbian 50 4 25 10"
