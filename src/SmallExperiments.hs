510b6dad29ce8be5bf2967ef51a60676963a8c80
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-04 21:23:53 +0000

    Changed experiment to get different results

--- src/SmallExperiments.hs
+++ src/SmallExperiments.hs
@@ -19,10 +19,10 @@ main = do
   avg2 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 4 25 5)
   print avg2
 
-  putStrLn "basinsGivenStdT2 Hebbian 50 4 25 10"
-  avg3 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 4 25 10)
+  putStrLn "basinsGivenStdT2 Hebbian 50 6 25 10"
+  avg3 <- liftM average $ replicateM 10 (basinsGivenStdT2 Hebbian 50 6 25 10)
   print avg3
 
-  putStrLn "basinsGivenStdT2 Storkey 50 4 25 10"
-  avg4 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 4 25 10)
+  putStrLn "basinsGivenStdT2 Storkey 50 6 25 10"
+  avg4 <- liftM average $ replicateM 10 (basinsGivenStdT2 Storkey 50 6 25 10)
   print avg4
