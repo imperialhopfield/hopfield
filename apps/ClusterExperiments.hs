b97cc68c9734c02787712ee7b926cbe5ea4b2434
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-05 19:49:23 +0000

    Removed code duplication and added start and stop parameteres in cluster experiments to be able to reproduce things

--- apps/ClusterExperiments.hs
+++ apps/ClusterExperiments.hs
@@ -16,34 +16,25 @@ import Hopfield.Util
 data ExpType = T1 | T2
         deriving (Eq, Show, Read)
 
-oneIterationT2 :: Int -> Int -> Double -> Int-> [(Double, Double)]
-oneIterationT2 networkSize clusterSize dev_step i = zip cs deviations
+
+oneIteration :: ExpType -> Int -> Int -> Double -> Double -> Double -> Int-> [(Double, Double)]
+oneIteration expType networkSize clusterSize start stop p_step i = zip cs values
   where
-    f x = evalRand (basinsGivenStdT2 Hebbian networkSize clusterSize mean x) (mkStdGen i)
-    unevaluated = map f deviations
+    f x = evalRand (evaluatedFunction x) (mkStdGen i)
+    unevaluated = map f values
     cs = unevaluated `using` parList rdeepseq
-    mean = networkSize ./ 2.0
-    deviations = [0.0, dev_step .. networkSize ./ 8.0]
+    values = [start, p_step .. stop]
+    evaluatedFunction = case expType of
+      T1 -> basinsGivenProbabilityT1 Hebbian networkSize clusterSize
+      T2 -> basinsGivenStdT2 Hebbian networkSize clusterSize (networkSize ./ 2.0)
 
 
-oneIterationT1 :: Int -> Int -> Double -> Int-> [(Double, Double)]
-oneIterationT1 networkSize clusterSize p_step i = zip cs probabilities
-  where
-    f x = evalRand (basinsGivenProbabilityT1 Hebbian networkSize clusterSize x) (mkStdGen i)
-    unevaluated = map f probabilities
-    cs = unevaluated `using` parList rdeepseq
-    probabilities = [0.0, p_step .. 0.6]
-
-
-performAndPrint :: ExpType -> Int -> Int -> Double -> Int ->IO ()
-performAndPrint expType neurons clusterSize step iterations = do
+performAndPrint :: ExpType -> Int -> Int -> Double -> Double -> Double -> Int ->IO ()
+performAndPrint expType neurons clusterSize start stop step iterations = do
   putStrLn $ "Experiment type" ++ show expType
   putStrLn $ "neurons  " ++ show neurons ++ "  cluster " ++ show clusterSize
   putStrLn $ "performed for " ++ show iterations ++ " iterations"
-  mapM_ print $ map (iterationFunc neurons clusterSize step) [0.. iterations]
-  where iterationFunc = case expType of
-                          T1 -> oneIterationT1
-                          T2 -> oneIterationT2
+  mapM_ print $ map (oneIteration expType neurons clusterSize start stop step) [0.. iterations]
 
 
 main :: IO ()
@@ -51,5 +42,5 @@ main = do
 
   args <- getArgs
   case args of
-    (t : n : c : iterations : step: _)-> performAndPrint (read t) (read n) (read c) (read step) (read iterations)
+    (t : n : c : iterations : start : stop : step: _)-> performAndPrint (read t) (read n) (read c) (read start) (read stop) (read step) (read iterations)
     _ -> error "invalid arguments"
