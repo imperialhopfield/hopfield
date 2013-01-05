32f45847d790cf69732b3ae8d4087ea11b535008
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-05 10:16:22 +0000

    One experiment in parallel to see how it goes. See ClusterExp for details

--- apps/ClusterExperiments.hs
+++ apps/ClusterExperiments.hs
@@ -2,8 +2,11 @@
 
 module Main where
 
+
 import Control.Monad (replicateM)
 import Control.Monad.Random
+import Control.Parallel.Strategies
+
 import Test.QuickCheck
 import Test.QuickCheck.Gen (unGen)
 
@@ -23,3 +26,15 @@ main = do
   putStrLn "T2 experiment with 1 cluster with no average but lists"
   let avgs =  replicate 10 $ experimentUsingT2NoAvg Hebbian 100 10
   printMList avgs (replicate 10 prettyList)
+
+
+  g <- getStdGen
+
+  let networkSize = 100
+      clusterSize = 10
+      mean = networkSize ./. (2 :: Int)
+      deviations = [0.0, 2.0, networkSize ./. (8 :: Int)]
+      f x = evalRand (basinsGivenStdT2 Hebbian networkSize clusterSize mean x) g
+      unevaluated = map f deviations
+      cs = unevaluated `using` parList rdeepseq
+  print cs
