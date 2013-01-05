cbb906fd298835712dc4fbb3c244c1c9761adaa6
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-05 16:26:42 +0000

    Added parsing of experiment type + supporting experiments of type T1 in parallel (for the first time)

--- apps/ClusterExperiments.hs
+++ apps/ClusterExperiments.hs
@@ -3,21 +3,21 @@
 module Main where
 
 
-import Control.Monad (replicateM)
+import Control.Monad
 import Control.Monad.Random
 import Control.Parallel.Strategies
 import System.Environment
 
-import Hopfield.Analysis
 import Hopfield.Clusters
 import Hopfield.Hopfield
-import Hopfield.Measurement
-import Hopfield.SuperAttractors
 import Hopfield.Util
 
 
-oneIteration :: Int -> Int -> Double -> Int-> [(Double, Double)]
-oneIteration networkSize clusterSize dev_step i = zip cs deviations
+data ExpType = T1 | T2
+        deriving (Eq, Show, Read)
+
+oneIterationT2 :: Int -> Int -> Double -> Int-> [(Double, Double)]
+oneIterationT2 networkSize clusterSize dev_step i = zip cs deviations
       where
         f x = evalRand (basinsGivenStdT2 Hebbian networkSize clusterSize mean x) (mkStdGen i)
         unevaluated = map f deviations
@@ -26,10 +26,22 @@ oneIteration networkSize clusterSize dev_step i = zip cs deviations
         deviations = [0.0, dev_step .. networkSize ./ 8.0]
 
 
-performAndPrintT2 :: Int -> Int -> Int -> Double -> IO ()
-performAndPrintT2 neurons clusterSize iterations dev_step = do
+oneIterationT1 :: Int -> Int -> Double -> Int-> [(Double, Double)]
+oneIterationT1 networkSize clusterSize p_step i = zip cs probabilities
+      where
+        f x = evalRand (basinsGivenProbabilityT1 Hebbian networkSize clusterSize x) (mkStdGen i)
+        unevaluated = map f probabilities
+        cs = unevaluated `using` parList rdeepseq
+        probabilities = [0.0, p_step .. 0.6]
+
+
+performAndPrint :: ExpType -> Int -> Int -> Int -> Double -> IO ()
+performAndPrint expType neurons clusterSize iterations step = do
     putStrLn $ "T2 neurons  " ++ show neurons ++ "  cluster " ++ show clusterSize
-    mapM_ print $ map (oneIteration neurons clusterSize dev_step) [0.. iterations]
+    mapM_ print $ map (iterationFunc neurons clusterSize step) [0.. iterations]
+    where iterationFunc = case expType of
+                          T1 -> oneIterationT1
+                          T2 -> oneIterationT2
 
 
 main :: IO ()
@@ -38,7 +50,7 @@ main = do
   args <- getArgs
 
   case args of
-    ("big": _)    -> performAndPrintT2 100 10 10 3.0
-    ("simple": _) -> performAndPrintT2 60  6  10 1.0
+    ("big":t: _)   -> performAndPrint (read t) 100 10 10 3.0
+    ("small":t: _) -> performAndPrint (read t) 60  6  10 1.0
     otherwise     -> putStrLn "Invalid argument"
 
