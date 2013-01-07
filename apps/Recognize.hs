0b8c7062a7c7277a6c4649a9848105dd55951c10
Niklas Hambüchen <mail@nh2.me>
2013-01-07 03:37:11 +0100

    Simplify recPic

--- apps/Recognize.hs
+++ apps/Recognize.hs
@@ -3,7 +3,6 @@
 module Main where
 
 import           Control.Monad
-import           Control.Monad.Random
 import qualified Data.Vector as V
 import           Options.Applicative
 
@@ -27,13 +26,15 @@ toPattern m (CBinaryPattern { cPattern = pat }) = V.fromList $ map (transformFun
 recPic :: Method -> (Int, Int) -> [FilePath] -> FilePath -> IO (Maybe FilePath)
 recPic method (width, height) imgPaths queryImgPath = do
   l@(_queryImg:_imgs) <- forM (queryImgPath:imgPaths) (\path -> loadPicture path width height)
-  gen <- getStdGen
   let queryPat:imgPats = map (toPattern method) l
-      runRandom r = evalRand r gen
-      result =  case method of
-          Hopfield   -> runRandom $ matchPattern (buildHopfieldData Storkey imgPats) queryPat
-          Boltzmann  -> Right $ runRandom $ matchPatternBoltzmann (runRandom $ buildBoltzmannData imgPats) queryPat
-          CBoltzmann -> Right $ matchPatternCBoltzmann (runRandom $ buildCBoltzmannData imgPats) queryPat
+
+  result <- case method of
+              Hopfield   -> matchPattern (buildHopfieldData Storkey imgPats) queryPat
+              Boltzmann  -> do d <- buildBoltzmannData imgPats
+                               Right <$> matchPatternBoltzmann d queryPat
+              CBoltzmann -> do d <- buildCBoltzmannData imgPats
+                               return . Right $ matchPatternCBoltzmann d queryPat
+
   return $ case result of
              Left _pattern -> Nothing -- TODO apply heuristic if we want (we want)
                                       -- only required for Hopfield
