2b2aaeb189e8e06dc449f0a6851a3a5b51d943a5
Wael Al Jishi <wa910@doc.ic.ac.uk>
2013-01-15 11:34:11 +0000

    Removed haskell-from-python and related

--- gui/haskell-from-python-2/Foo.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-module Foo where
- 
-import Foreign.C.Types
- 
-foreign export ccall hs_fact :: CInt -> CInt
-hs_fact :: CInt -> CInt
-hs_fact n = product [1..n]
--- gui/haskell-from-python-2/run.py
+++
@@ -1,5 +0,0 @@
-import example
-import sys
-example.py_init(sys.argv)
-print example.fact(5)
-example.py_exit()
